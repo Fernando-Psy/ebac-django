46786fe69ef36835e669b37010bfe5c3e9164c89
Fernando-Psy <fcesarjunior@gmail.com>
2025-05-27 07:38:13 -0300

    Modificar o template

--- blog/admin.py
+++ blog/admin.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
 from .models import Post
 
+@admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'slug', 'status', 'created')
     list_filter = ('status',)
     search_fields = ('title', 'body', 'author__username')
     prepopulated_fields = {'slug': ('title',)}
-
-admin.site.register(Post, PostAdmin)
